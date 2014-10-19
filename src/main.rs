419212b2ca7d5e95571d2e31dc4c77382e1ff5b4
Caleb Jones <bionicle.dragon@gmail.com>
2014-10-19 12:54:46 -0400

    Cleanup code slightly
    Increase buffer size
    Remove redundant code
    - Remove input buffer
    - Remove dead assignment
    Change envelope function to reduce clicking

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use portaudio::*;
 
 fn main() -> () {
 
-    let stream = init_portaudio_with_defaults_f32(1i32, 44100., 5u32);
+    let stream = init_portaudio_with_defaults_f32(1i32, 44100., 64u32);
 
     let notes = [1760f32, 1567.98f32, 1396.91f32, 1318.51f32, 1174.66f32, 
                 1046.50f32, 987.77f32, 880f32, 783.99f32, 698.46f32, 659.26f32, 
@@ -18,7 +18,7 @@ fn main() -> () {
                 98f32, 87.31f32, 82.41f32, 73.42f32, 65.41f32, 61.74f32, 55f32, 49f32, 
                 43.65f32, 41.20f32, 36.71f32, 32.70f32, 30.87];
     for i in notes.iter(){
-        play_sine_wave(stream, *i as f32, 0.4, 44100., 5u32);
+        play_sine_wave(stream, *i as f32, 0.4, 44100., 64u32);
     }
 
     terminate_portaudio_f32(stream);
@@ -40,22 +40,6 @@ fn init_portaudio_with_defaults_f32(channel_count: i32, sample_rate: f64, buffer
     println!("Portaudio type id : {}",
              pa::host_api_type_id_to_host_api_index(types::PaCoreAudio) as int);
 
-
-    let def_input = pa::get_default_input_device();
-
-
-    if pa::get_device_info(def_input).is_none() {
-       println!("error");
-    }
-
-    // PaStream test :
-    let stream_params  = types::PaStreamParameters {
-        device : def_input,
-        channel_count : 2,
-        sample_format : types::PaFloat32,
-        suggested_latency : pa::get_device_info(def_input).unwrap().default_low_input_latency
-    };
-
     let def_output = pa::get_default_output_device();
     println!("name : {}", pa::get_device_info(def_output).unwrap().name);
 
@@ -68,16 +52,16 @@ fn init_portaudio_with_defaults_f32(channel_count: i32, sample_rate: f64, buffer
 
     let mut stream : pa::PaStream<f32> = pa::PaStream::new(types::PaFloat32);
 
-    let mut err = stream.open(Some(&stream_params), Some(&stream_params_out), sample_rate, buffer_size, types::PaClipOff);
+    let err = stream.open(None, Some(&stream_params_out), sample_rate, buffer_size, types::PaClipOff);
     
     println!("Portaudio Start error : {}", pa::get_error_text(err));
-
-    err = stream.start();
+    
+    let _ = stream.start();
 
     stream
 }
 
-fn play_sine_wave(stream: pa::PaStream<f32>, frequency: f32, duration: f32,  sample_rate: f32,  buffer_size : u32) {
+fn play_sine_wave(stream: pa::PaStream<f32>, frequency: f32, duration: f32,  sample_rate: f32,  buffer_size: u32) {
     //contiguous counter
     let mut x = 0i;
     for _ in range(0i, (duration * sample_rate / buffer_size as f32).round() as int){
@@ -95,19 +79,19 @@ fn play_sine_wave(stream: pa::PaStream<f32>, frequency: f32, duration: f32,  sam
         stream.write(buffer, buffer_size);
     }
 }
-// uses 10* 2^-x * atan(x), x in radians.
+// uses 10* 2^(-(x^2)) * atan(x^2), x over [0, 3]
 fn amplitude_function(duration : f32, sample_rate: f32, pos : int ) -> f32 {
-    let x = 1. * ((pos as f32 / (sample_rate * duration))-1.);
-    10f32 * 2f32.powf(-1. * x) * x.atan()
+    //let x = 1. * ((pos as f32 / (sample_rate * duration))-1.);
+    let x = 3.0 * (pos as f32 / (sample_rate * duration));
+    let x = x*x;
+    3f32 * 2f32.powf(-1. * x) * x.atan()
 }
 
 fn terminate_portaudio_f32(mut stream : pa::PaStream<f32>){
-    let mut err = types::PaNotInitialized;
-    err = stream.close();
+    let err = stream.close();
     println!("Portaudio Close stream error : {}", pa::get_error_text(err));
 
     println!("");
-
 
     println!("Portaudio terminate error : {}", pa::get_error_text(pa::terminate()));
 }
